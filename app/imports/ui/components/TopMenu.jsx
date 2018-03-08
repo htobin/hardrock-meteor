674dca3b32adb338f453ae3c27c29d72dcc5f76d
Hoku <htobin09@gmail.com>
2018-03-07 22:16:55 -1000

    inline hard styling

--- app/imports/ui/components/TopMenu.jsx
+++ app/imports/ui/components/TopMenu.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Menu, Image } from 'semantic-ui-react';
 
-
+const menuColor = { color: '#ccc' };
 export default class TopMenu extends React.Component {
   render() {
     return (
         <Menu borderless className="topmenu">
           <Image src="http://courses.ics.hawaii.edu/ics314f17/morea/ui-frameworks/hard-rock-logo.png"/>
-          <Menu.Item position={'right'}>CAFES</Menu.Item>
-          <Menu.Item>HOTELS</Menu.Item>
-          <Menu.Item>CASINOS</Menu.Item>
-          <Menu.Item>LIVE</Menu.Item>
-          <Menu.Item>SHOP</Menu.Item>
-          <Menu.Item>BLOG</Menu.Item>
-          <Menu.Item>REWARDS</Menu.Item>
+          <Menu.Item style={menuColor} position={'right'}>CAFES</Menu.Item>
+          <Menu.Item style={menuColor}>HOTELS</Menu.Item>
+          <Menu.Item style={menuColor}>CASINOS</Menu.Item>
+          <Menu.Item style={menuColor}>LIVE</Menu.Item>
+          <Menu.Item style={menuColor}>SHOP</Menu.Item>
+          <Menu.Item style={menuColor}>BLOG</Menu.Item>
+          <Menu.Item style={menuColor}>REWARDS</Menu.Item>
         </Menu>
     );
   }
